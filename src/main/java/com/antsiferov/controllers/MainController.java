8438fef39b39f148b6477954cd67e8a77feb640f
Sergei Anciferov <sergozas@yandex.ru>
2022-02-25 15:25:21 +0300

    test

--- src/main/java/com/antsiferov/controllers/MainController.java
+++ src/main/java/com/antsiferov/controllers/MainController.java
@@ -1,7 +1,6 @@
 package com.antsiferov.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,7 +8,7 @@ public class MainController {
 
     @GetMapping({"/", "/home"})
     public String home() {
-        return "/homepage";
+        return "homepage";
     }
 
 }
