df8c4e147ddf47d9af335d4330f8d3f70561580f
Sergei Anciferov <sergozas@yandex.ru>
2022-02-28 02:46:49 +0300

    test

--- src/main/java/com/antsiferov/entity/Post.java
+++ src/main/java/com/antsiferov/entity/Post.java
@@ -1,6 +1,8 @@
 package com.antsiferov.entity;
 
 import lombok.Data;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.persistence.*;
 import java.text.DateFormat;
@@ -34,15 +36,18 @@ public class Post {
     @Column
     private String pictureURL;
 
+    @Column
+    private String author;
+
     public Post(String subject, String text){
-        DateFormat df = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
-        this.date = df.format(new Date());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        this.author = auth.getName();
+        this.date = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss").format(new Date());
         this.text = text;
         this.subject = subject;
         this.likes = 0;
         this.dislikes = 0;
     }
-
 
     public Post() {
 
