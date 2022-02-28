7edaa2893854de10af70f306bbd380feb42528b1
Sergei Anciferov <sergozas@yandex.ru>
2022-02-28 23:59:03 +0300

    test

--- src/main/java/com/antsiferov/controllers/FeedController.java
+++ src/main/java/com/antsiferov/controllers/FeedController.java
@@ -1,13 +1,26 @@
 package com.antsiferov.controllers;
 
+import com.antsiferov.entity.Post;
+import com.antsiferov.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class FeedController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/feed")
-    public String feed() {
+    public String feed(Model model) {
+        List<Post> posts = postRepository.findAll();
+        Collections.reverse(posts);
+        model.addAttribute("posts", posts);
         return "feed";
     }
 
--- src/main/java/com/antsiferov/repository/PostRepository.java
+++ src/main/java/com/antsiferov/repository/PostRepository.java
@@ -1,10 +1,11 @@
 package com.antsiferov.repository;
 
 import com.antsiferov.entity.Post;
-import com.antsiferov.entity.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 
-
+    List<Post> findAll();
 }
